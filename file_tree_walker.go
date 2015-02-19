1967667a896a4474e2f3298250c2482a6c1a0d6f
Behram Mistree <bmistree@gmail.com>
2015-02-19 00:32:47 -0800

    Initial commit for walking folders.

---
+++ file_tree_walker.go
@@ -0,0 +1,69 @@
+package sgrep
+
+import "io/ioutil"
+import "path"
+import "fmt"
+
+type Directory struct {
+    // The name of this directory relative to its parent directory.
+    // Eg., for structure a/b/c/, would have a directory with name
+    // a, name b, and name c.
+    Name string
+    Files []string
+    Directories []* Directory
+}
+
+func PrettyPrint(dir *Directory) {
+	dir.pretty_print_helper(0)
+}
+
+func (dir *Directory) pretty_print_helper(indentation_level uint32) {
+
+    indent_str := ""
+    for i := uint32(0); i < indentation_level; i++ {
+        indent_str += "\t"
+    }
+    fmt.Println(indent_str + dir.name)
+    
+    for _, filename := range dir.files {
+        fmt.Println(indent_str + filename)
+    }
+        
+    for _, sub_directory := range dir.directories {
+        sub_directory.pretty_print_helper(indentation_level + 1)
+    }
+}
+
+
+/**
+  @param dir_to_walk The directory that we want to list all
+  subdirectories of.
+
+  @returns A Directory struct containing all subfiles and folders.
+*/
+func walk_folder( dir_to_walk string) * Directory {
+    dir_contents_list, err := ioutil.ReadDir(dir_to_walk)
+    if err != nil {
+        panic ("IOError when reading directory " + dir_to_walk)
+    }
+
+	root := new(Directory)
+    root.name, _ = path.Split(dir_to_walk)
+    
+	for _, file_info := range dir_contents_list {
+        fully_qualified_path := path.Join(dir_to_walk,file_info.Name())
+        if err != nil {
+            panic(
+                "Could not stat file or folder named " +
+                    fully_qualified_path)
+        }
+        
+        if file_info.IsDir() {
+            sub_directory := walk_folder(fully_qualified_path)
+            root.directories = append(root.directories,sub_directory)
+        } else {
+            root.files = append(root.files, file_info.Name())
+        }
+    }
+    return root
+}
