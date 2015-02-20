84b4c3bac768bf930ee7ea8932e5aab1712366b8
Behram Mistree <bmistree@gmail.com>
2015-02-19 16:44:27 -0800

    Added rule constructor for testing.

--- lib/rule.go
+++ lib/rule.go
@@ -14,8 +14,16 @@ type Rule struct {
 }
 
 
+func ConstructRule(containing_file_abs_path, raw_rule_text string) *Rule {
+    r1 :=  Rule {}
+    r1.containing_file_abs_path = containing_file_abs_path
+    r1.raw_rule_text = raw_rule_text
+    return &r1
+}
+
+
 // returns true if this rule filters (ie., says not to look in) file named filename.
-func (rule* Rule) file_filterer(filename string) bool {
+func (rule* Rule) FileFilterer(filename string) bool {
 
     did_match, err :=  filepath.Match(rule.raw_rule_text,filename)
     if err != nil {
