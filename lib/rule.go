722c1a3162759019c0e134329e298b3ec0933f33
Behram Mistree <bmistree@gmail.com>
2015-02-24 13:59:17 -0800

    Started adding code for parsing rules from sgrep files.  Still need to test code and to actually add harness that reads sgrep files.

--- lib/rule.go
+++ lib/rule.go
@@ -1,6 +1,9 @@
 package sgrep
 
 import "path/filepath"
+import "strings"
+
+const COMMENT_STRING string = "#"
 
 /**
 Basic rule struct: when we encounter an sgrep file
@@ -36,4 +39,27 @@ func (rule* Rule) FileFilterer(filename string) bool {
         return true
     }
     return false
+}
+
+/**
+@param sgrepAbsFilename --- The absolute path for the file that
+contains the line that we're about to parse into a rule.
+
+@param text --- The line that we're trying to turn into an sgrep rule
+
+@returns nil if text does not produce a valid sgrep rule (eg., empty
+line, commented-out rule, etc.)  Otherwise, returns rule.
+*/
+func ParseRule(sgrepAbsFilename, text string) *Rule{
+    // ignore comments
+    commentIndex := strings.Index(text, COMMENT_STRING)
+    if commentIndex != -1 {
+        text = text[0:commentIndex]
+    }
+    text = strings.TrimSpace(text)
+    // ignore blank lines
+    if text == "" {
+        return nil
+    }
+    return ConstructRule(sgrepAbsFilename, text)
 }
