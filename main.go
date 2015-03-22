d0e7bf00f85495bdd784a06c0378f84b97fb359a
Behram Mistree <bmistree@gmail.com>
2015-03-22 13:42:01 -0700

    Added a few comments, explaining basic structure of sgrep binary.

--- main.go
+++ main.go
@@ -11,13 +11,34 @@ import "path/filepath"
 // FIXME: probably more generic ways to do this (eg., for windows)
 const GREP_BIN_PATH string = "grep"
 
+/**
+Basic structure:
+
+  1) Check arguments and based on arguments, create directory objects
+
+  2) Directory objects read through all .sgrep files and produce a
+  list of files that we should grep over.
+
+  3) Exec a grep command with the list of files generated from
+  directory objects in #2, then redirect grep's stdout and stderr this
+  process's stdout and stderr.
+*/
 func main() {
 	args := parseArgs()
-	// a list of files to grep in
+	// a list of files to actually exec over
 	var filesToGrepOver []string
+
+	// List of files passed in at command line by user to sgrep
+	// over.  For instance, issuing "sgrep something *txt", in a
+	// directory with files a.txt and b.txt in it would produce
+	// filesToCheckWhetherToGrepOver with a.txt and b.txt.  We
+	// later produce directory objects that determine whether to
+	// filter the files when passing them into grep exec.
 	var filesToCheckWhetherToGrepOver []string
 
-	// for all files and folders in whereToGrep, find folders,
+	// for all files and folders passed into command line:
+	//   1) build directory objects from folders
+	//   2) copy all passed in files into filesToCheckWhetherToGrepOver
 	for _, toGrepOver := range args.whereToGrep {
 		file, err := os.Open(toGrepOver)
 		if err != nil {
@@ -43,6 +64,9 @@ func main() {
 			dirFiles := dir.ListNonRuleFilteredFiles()
 			filesToGrepOver = append(filesToGrepOver, dirFiles...)
 		} else {
+			// later will run .sgrep filters over these
+			// lists via call to
+			// sgreplib.NonFilteredFilesToGrepOver
 			filesToCheckWhetherToGrepOver = append(
 				filesToCheckWhetherToGrepOver, toGrepOver)
 		}
